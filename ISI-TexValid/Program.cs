3e87af64c0bf99c4a1e0bd4559ad61704921f87a
ChelovekTM <attilamarkkeri2009@gmail.com>
2025-12-26 12:21:34 +0100

    Added .dds support along with the libraries needed for it work. TODO: Testing

--- ISI-TexValid/Program.cs
+++ ISI-TexValid/Program.cs
@@ -14,18 +14,18 @@ namespace ISI_TexValid
             string path = Environment.CurrentDirectory;
             string[] texturesBmp = Directory.GetFiles(path, "*.bmp");
             string[] texturesTga = Directory.GetFiles(path, "*.tga");
-            // string[] texturesDds = Directory.GetFiles(path, "*.dds");
+            string[] texturesDds = Directory.GetFiles(path, "*.dds");
 
             // Load textures
             TextureChecker[] texturesBitmap = Array.ConvertAll(texturesBmp, texture => new BitmapChecker(texture));
             TextureChecker[] texturesTarga = Array.ConvertAll(texturesTga, texture => new TargaChecker(texture));
-            // TextureChecker[] texturesDirectDraw = Array.ConvertAll(texturesDds, texture => new DirectDrawChecker(texture));
+            TextureChecker[] texturesDirectDraw = Array.ConvertAll(texturesDds, texture => new DirectDrawChecker(texture));
 
             // Process invalid textures
             List<TextureChecker> invalidTextures = new List<TextureChecker>();
             ProcessTextures(texturesBitmap, invalidTextures);
             ProcessTextures(texturesTarga, invalidTextures);
-            // ProcessTextures(texturesDirectDraw, invalidTextures);
+            ProcessTextures(texturesDirectDraw, invalidTextures);
 
             // Order invalid textures by error level so that the more severe errors appear first in the output
             List<TextureChecker> orderedTextures = invalidTextures.OrderByDescending(texture => texture.errorLevel).ToList();
@@ -51,6 +51,14 @@ namespace ISI_TexValid
                 {
                     File.AppendAllText("output.txt", $"Info: {targa.fileName} is {TargaChecker.CompressionType(targa)}\n");
                 }
+                if (texture is DirectDrawChecker directDraw1 && !DirectDrawChecker.HasMipMaps(directDraw1))
+                {
+                    File.AppendAllText("output.txt", $"Info: {directDraw1.fileName} has no mip-maps\n");
+                }
+                if (texture is DirectDrawChecker directDraw2 && !DirectDrawChecker.IsCompressed(directDraw2))
+                {
+                     File.AppendAllText("output.txt", $"Info: {directDraw2.fileName} is uncompressed\n");
+                }
                 File.AppendAllText("output.txt", "---------------------------------------------------------------------------------------------\n");
             }
         }
@@ -66,7 +74,9 @@ namespace ISI_TexValid
             {
                 if (!TextureChecker.ValidDimensions(texture) | !TextureChecker.FileNameLength(texture) |
                     (texture is BitmapChecker bitmap && !BitmapChecker.ValidPixelFormat(bitmap)) | 
-                    (texture is TargaChecker targa && !TargaChecker.Uncompressed(targa)))
+                    (texture is TargaChecker targa && !TargaChecker.Uncompressed(targa)) |
+                    (texture is DirectDrawChecker directDraw && (!DirectDrawChecker.HasMipMaps(directDraw)) | 
+                    (!DirectDrawChecker.IsCompressed(directDraw))))
                 {
                     invalidTextures.Add(texture);
                 }
---
+++ ISI-TexValid/TextureProcessors/DirectDrawChecker.cs
@@ -0,0 +1,60 @@
+﻿using DDSParser; // Written by jmschrack: https://github.com/jmschrack/DDS-Image-Wrapper.git
+
+namespace ISI_TexValid.TextureProcessors
+{
+    internal class DirectDrawChecker : TextureChecker
+    {
+        public int mipMapCount;
+        public string compressionType;
+
+        public DirectDrawChecker(string texture) : base(texture)
+        {
+            byte[] rawDDS = System.IO.File.ReadAllBytes(texture);
+            DDSImage DDS = new DDSImage(rawDDS);
+            width = DDS.Width;
+            height = DDS.Height;
+            mipMapCount = DDS.MipMapCount;
+
+            if (DDS.HasFourCC)
+            {
+                compressionType = DDS.GetPixelFormatFourCC().ToString();
+            }
+            else
+            {
+                compressionType = "Uncompressed RGB";
+            }
+        }
+
+        /// <summary>
+        /// Checks whether the DirectDraw texture has 2 or more mip-maps
+        /// </summary>
+        /// <remarks>DirectDraw textures always have at least 1 mip-map, which is the original texture.</remarks>
+        /// <param name="directDraw">The DirectDraw texture</param>
+        /// <returns>True if it has 2 or more mip-maps, false if it doesn't</returns>
+        public static bool HasMipMaps(DirectDrawChecker directDraw)
+        {
+            if (directDraw.mipMapCount > 1)
+            {
+                return true;
+            }
+
+            directDraw.errorLevel += 1;
+            return false;
+        }
+
+        /// <summary>
+        /// Checks whether the DirectDraw texture is compressed or uncompressed
+        /// </summary>
+        /// <param name="directDraw">The DirectDraw texture</param>
+        /// <returns>True if it's compressed, false if it's not</returns>
+        public static bool IsCompressed(DirectDrawChecker directDraw)
+        {
+            if (directDraw.compressionType != "Uncompressed RGB")
+            {
+                return true;
+            }
+            directDraw.errorLevel += 1;
+            return false;
+        }
+    }
+}
